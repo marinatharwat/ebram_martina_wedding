bba5427d0b1071077d1c9c33080f0f52902d01a0
Marina Tharwat <marina.tharwat1999@gmail.com>
2026-08-14 02:19:56 +0300

    update2

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2280404397"
+    serviceWorkerVersion: "2318708998"
   }
 });
